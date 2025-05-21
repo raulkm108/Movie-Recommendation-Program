3a1b8e0ddb6bca98b307c502320be714f6ee1764
raulkm108 <raulkm.civil@gmail.com>
2025-05-21 10:03:21 -0300

    Inicial load of main.py with dataset loading and validation
    Creation of movies which reads the csv file and converts it into a dataframe and setting error message in case it doesn't exist. Checking the columns  if they match the required format and filling the blank spaces in the descriptions to avoid errors

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+try:
+    movies = pd.read_csv("movies.csv")
+except FileNotFoundError:
+    print("⚠️ File 'movies.csv' not found. Please make sure it is in the same directory.")
+    exit()
+
+required_colums = {'title', 'description'}
+if not required_colums.issuebset(movies.colums):
+    print("⚠️ The dataset must contain the colums: 'title and 'description'.")
+    exit()
+movies['description'] = movies['description'].fillna('')
