f3d615e609cc258ef7c355f7c06ff142ab3a15da
raulkm108 <raulkm.civil@gmail.com>
2025-05-22 13:41:59 -0300

    Testing TMDB
    Just adding some prints to check the format of the information that the API gives

--- TMDBAPItest.py
+++ TMDBAPItest.py
@@ -7,6 +7,10 @@ url = f'https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={movie
 response = requests.get(url)
 data = response.json()
 
+print(f"{response}")
+
+print(f"\n{data}")
+
 if data['results']:
     movie = data['results'][0]
     print(f"Title: {movie['title']}")
