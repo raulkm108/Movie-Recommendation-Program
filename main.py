1670c762f79cf781c4e4bad28f14134188e4b9c1
raulkm108 <raulkm.civil@gmail.com>
2025-05-22 09:00:28 -0300

    Defining main
    Defining the main function to organize things better

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+def main():
+    movies = load_data("movies.csv")
+    vectorizer = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = vectorizer.fit_transform(movies['description'])
+
+    input_title = input("\n Enter a movie title: ").strip()
+    titles = movies['title'].tolist()
+
+    if input_title.lower() not in [t.lower() for t in titles]:
+        suggestion = suggest_similar_title(input_title, titles)
+        if suggestion:
+            input_title = suggestion
+        else:
+            print("❌ No match found or cancelled.")
+            return
+
+movie_index = movies[movies['title'].str.lower() == input_title.lower()].index[0]
+recommendation = get_recommendations(movie_index, tfidf_matrix, movies)
+
+print(f"\n Recommended movies based on '{input_title}':")
+for title, score in recommendation:
+    print(f" {title} (score: {score:.6f})")
+
+
 def load_data(path_csv)
     try:
         movies = pd.read_csv(path_csv)
