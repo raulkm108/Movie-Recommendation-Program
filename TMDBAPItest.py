dd1ab19aada8b9565b3b205d151e1e4538bb1342
raulkm108 <raulkm.civil@gmail.com>
2025-05-22 12:45:52 -0300

    Testing the API
    Creating a script to check if the API from TMDB is working

---
+++ TMDBAPItest.py
@@ -0,0 +1,15 @@
+import requests
+
+API_KEY = '8e5204ead69396cef3240e5175bb098e'
+movie_title = 'inception'
+url = f'https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={movie_title}'
+
+response = requests.get(url)
+data = response.json()
+
+if data['results']:
+    movie = data['results'][0]
+    print(f"Title: {movie['title']}")
+    print(f"Description: {movie['overview']}")
+else:
+    print("Movie not Found.")
