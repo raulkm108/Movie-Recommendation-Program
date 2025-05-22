83a6ebf2903bce3e4137f9527bcdb130b2c308a0
raulkm108 <raulkm.civil@gmail.com>
2025-05-22 09:09:12 -0300

    Defining get_recommendations
    Defining the function get_recommendations which was already there, just defined it as an actual function

--- main.py
+++ main.py
@@ -37,27 +37,13 @@ def load_data(path_csv)
         print(f"⚠️ The dataset must contain 'title' and 'description'.")
         exit()
     movies['description'] = movies['description'].fillna('')
+    return movies
 
-tfidf_matrix = vectorizer.fit_transform(movies['description'])
+def get_recommendations(movie_index, tfidf_matrix, movies, n=5):
+    cosine_similarities = cosine_similarity(tfidf_matrix[movie_index], tfidf_matrix).flatten()
+    cosine_similarities = np.round(cosine_similarities, 6)
+    similar_index = cosine_similarities.argsort()[::-1][1:n + 1]
+    return [(movies.iloc[i]['title'], cosine_similarities[i]) for i in similar_index]
 
-# print("✅ TF-IDF matrix created. Shape:", tfidf_matrix.shape) >> printing the amount of descriptions and the amount of "important" vectors
-
-movie_title = input("\n🎬 Enter a movie title: ").strip().lower()
-
-
-if movie_title not in movies['title'].str.lower().values:
-    print(f"❌ Movie '{movie_title}' not found in the dataset.")
-    exit()
-
-movie_index = movies[movies['title'].str.lower() == movie_title].index[0]
-
-cosine_similarities = cosine_similarity(tfidf_matrix[movie_index], tfidf_matrix).flatten()
-cosine_similarities = np.round(cosine_similarities, 6)
-
-similar_indices = cosine_similarities.argsort()[::-1][1:6]
-
-print("\n🎯 Recommended movies:")
-for idx in similar_indices:
-    title = movies.iloc[idx]['title']
-    score = cosine_similarities[idx]
-    print(f"🔹 {title} (score: {score:.6f})")
+if __name__ == "__main__":
+    main()
