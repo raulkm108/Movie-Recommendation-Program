1f0b86ac325e2a8d69b5611fff2a71ec383506f2
raulkm108 <raulkm.civil@gmail.com>
2025-05-22 09:21:22 -0300

    Creating suggest_similar_title
    This function allows typos on the input and suggests similar titles, then the user can interact to keep the search

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import difflib
 
 def main():
     movies = load_data("movies.csv")
@@ -19,15 +20,14 @@ def main():
             print("❌ No match found or cancelled.")
             return
 
-movie_index = movies[movies['title'].str.lower() == input_title.lower()].index[0]
-recommendation = get_recommendations(movie_index, tfidf_matrix, movies)
+    movie_index = movies[movies['title'].str.lower() == input_title.lower()].index[0]
+    recommendation = get_recommendations(movie_index, tfidf_matrix, movies)
 
-print(f"\n Recommended movies based on '{input_title}':")
-for title, score in recommendation:
-    print(f" {title} (score: {score:.6f})")
+    print(f"\n Recommended movies based on '{input_title}':")
+    for title, score in recommendation:
+        print(f" {title} (score: {score:.6f})")
 
-
-def load_data(path_csv)
+def load_data(path_csv):
     try:
         movies = pd.read_csv(path_csv)
     except FileNotFoundError:
@@ -38,6 +38,20 @@ def load_data(path_csv)
         exit()
     movies['description'] = movies['description'].fillna('')
     return movies
+
+def suggest_similar_title(input_title, title_list):
+    matches = difflib.get_close_matches(input_title, title_list, n=5, cutoff=0.5)
+    if matches:
+        print("\n Movie not found, Did you mean one of these?")
+        for i, match in enumerate(matches, 1):
+            print(f"{i}. {match}")
+        try:
+            choice = int(input("\nEnter the number of the correct movie (0 to cancel): "))
+            if 1 <= choice <= len(matches):
+                return matches[choice - 1]
+        except ValueError:
+            pass
+    return None
 
 def get_recommendations(movie_index, tfidf_matrix, movies, n=5):
     cosine_similarities = cosine_similarity(tfidf_matrix[movie_index], tfidf_matrix).flatten()
