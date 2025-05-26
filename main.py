00ca8f06a93ffac74e1c6a78bab544e95431af28
raulkm108 <raulkm.civil@gmail.com>
2025-05-26 08:55:49 -0300

    Function stemeed_tokenizer
    Creating a new function to improve comparison through Stemming which just cuts words so they return to their "root" form and makes the comparison more accurate

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ def get_popular_movies(API_KEY, pages=10):
             movies.extend(results)
     return movies
 
+def stemmed_tokenizer(text):
+    
 def get_recommendations(input_title, API_KEY, n=5):
     movie_id = search_tdmb_movie(input_title, API_KEY)
     if not movie_id:
