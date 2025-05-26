fa42ac0f71f79093eba4b98d46c538ead3163c91
raulkm108 <raulkm.civil@gmail.com>
2025-05-26 09:04:35 -0300

    Downloaded punkt_tab
    Download more features

--- downloadnltk.py
+++ downloadnltk.py
@@ -1,3 +1,3 @@
-import downloadnltk
-downloadnltk.download('punkt') # Necessário para tokenização
+import nltk
+nltk.download('punkt_tab') # Necessário para tokenização
 # nltk.download('wordnet') # Se fosse usar WordNetLemmatizer
